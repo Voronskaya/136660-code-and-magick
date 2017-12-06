39fd9a39656ef13a1832eb51c159b581bd30fd24
Nina <ventio-r@rambler.ru>
2017-12-06 17:06:26 +0300

    Применила делегирование:событие keydown,добавила ф-ии изменения цвета svg, фаервола

--- js/setup.js
+++ js/setup.js
@@ -4,15 +4,21 @@ var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Крис�
 var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 var ESC_KEYCODE = 27;
 var ENTER_KEYCODE = 13;
 var setupSimilarElement = document.querySelector('.setup-similar');
 var setupSimilarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
-var setupElement = document.querySelector('.setup');
 var setupOpenElement = document.querySelector('.setup-open');
-var setupCloseElement = document.querySelector('.setup-close');
-var setupUserNameElement = document.querySelector('.setup-user-name');
+var setupElement = document.querySelector('.setup');
+var setupOpenIconElement = setupOpenElement.querySelector('.setup-open-icon');
+var setupCloseElement = setupElement.querySelector('.setup-close');
+var setupUserNameElement = setupElement.querySelector('.setup-user-name');
+var wizardCoat = setupElement.querySelector('.setup-wizard .wizard-coat');
+var wizardEyes = setupElement.querySelector('.setup-wizard .wizard-eyes');
+var setupFireballWrap = setupElement.querySelector('.setup-fireball-wrap');
+var setupPWizardWrapElement = setupElement.querySelector('.setup-wizard-wrap');
 
 setupSimilarElement.classList.remove('hidden');
 
@@ -66,38 +72,51 @@ var renderWizardList = function () {
   setupSimilarListElement.appendChild(fragment);
 };
 
-var checkFocusUserName = function () {
-  return document.activeElement === setupUserNameElement;
+var openPopup = function () {
+  setupElement.classList.remove('hidden');
+};
+
+var closePopup = function () {
+  setupElement.classList.add('hidden');
 };
 
 var keyDownHandler = function (evt) {
   if (evt.keyCode === ENTER_KEYCODE) {
-    openPopup();
+    if (document.activeElement === setupOpenIconElement) {
+      openPopup();
+    } else if (document.activeElement === setupCloseElement) {
+      closePopup();
+    }
   } else if (evt.keyCode === ESC_KEYCODE) {
-    if (!checkFocusUserName()) {
+    if (document.activeElement !== setupUserNameElement) {
       closePopup();
     }
   }
 };
 
-var openPopup = function () {
-  setupElement.classList.remove('hidden');
-  document.addEventListener('keydown', keyDownHandler);
+var svgClickHandler = function (evt) {
+  if (wizardCoat === evt.target) {
+    wizardCoat.style.fill = getRandomElement(COAT_COLORS);
+  } else if (wizardEyes === evt.target) {
+    wizardEyes.style.fill = getRandomElement(EYES_COLORS);
+  }
 };
 
-var closePopup = function () {
-  setupElement.classList.add('hidden');
-  document.removeEventListener('keydown', keyDownHandler);
+var setupFireballClickHandler = function () {
+  setupFireballWrap.style.background = getRandomElement(FIREBALL_COLORS);
 };
+
+document.addEventListener('keydown', keyDownHandler);
 
 setupOpenElement.addEventListener('click', function () {
   openPopup();
 });
 
-setupOpenElement.addEventListener('keydown', keyDownHandler);
-
 setupCloseElement.addEventListener('click', function () {
   closePopup();
 });
+// не знаю как назвать слушателeй
+setupPWizardWrapElement.addEventListener('click', svgClickHandler);
+setupFireballWrap.addEventListener('click', setupFireballClickHandler, true);
 
 renderWizardList();
